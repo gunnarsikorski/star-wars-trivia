20c5a2cbaf7ed16db9eb09f58e5ece5419494f56
gunnarsikorski <gunnarsikorski@gmail.com>
2020-10-08 11:32:54 -0500

    test to master

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+1. Modal on launch - explaining how the game works
+2. Close button on modal and button to reopen it
+3. Button to begin the game and brings up the first question
+4. Input box to type in answer and button to submit
+5. Answer pops up after guess is submitted
+6. Button for the next question to come up
+7. Different modal to pop up and show score?
+*/
