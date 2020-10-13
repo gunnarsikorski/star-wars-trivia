648521d074cc0350961fe0214b20d78b257adf17
gunnarsikorski <gunnarsikorski@gmail.com>
2020-10-13 14:50:51 -0500

    cleanup

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ closeModalButton.addEventListener('click', closeModal);
 
 //________________________________________________________________________
 
-/// Question Button ///
+/// Begin Button ///
 
 const questionButton = document.querySelector('#questionButton');
 let questionOne = document.querySelector('#q1');
@@ -34,7 +34,7 @@ questionButton.addEventListener('click', revealQuestion);
 
 //________________________________________________________________________
 
-/// Answer field ///
+/// Input and Answer field ///
 
 const form = document.querySelector('form');
 const answerInput = document.querySelector('input[type=text]');
@@ -148,9 +148,6 @@ form.addEventListener('submit', (event) => {
 		rightGif.style.display = 'block';
 	} else {
 		wrongGif.style.display = 'block';
-
-		// answerOne.innerText = triviaArray[].answer;
-		// answerOne.style.display = 'block';
 	}
 });
 
@@ -223,11 +220,12 @@ function cleanPage() {
 	}
 }
 
-// works for now, need to implement loop on array
+// works for now, need to implement loop on array somehow
 
 //________________________________________________________________________
 
-/// Finish Button ///
+/// Finish Button leading to thanks modal///
+
 const thanksModal = document.querySelector('#thanksModal');
 const replayButton = document.querySelector('#thanksModalReplay');
 
@@ -243,6 +241,8 @@ finishButton.addEventListener('click', openThanksModal);
 // replayButton.addEventListener('click', closeThanksModal)
 
 //________________________________________________________________________
+
+/// Array containing question and answers ///
 
 const triviaArray = [
 	{
@@ -332,48 +332,6 @@ const triviaArray = [
 	},
 ];
 
-// SCORE SOMEWHERE??? //
+//_______________________________________________________________________________
 
-/*
-1. Modal on launch - explaining how the game works
-2. Close button on modal and button to reopen it
-3. Button to begin the game and brings up the first question
-4. Input box to type in answer and button to submit
-5. Answer pops up after guess is submitted
-6. Button for the next question to come up
-7. Different modal to pop up and show score?
-*/
 
-// Trivia questions:
-
-/*
-1.  Q: Who was Darth Vader before turning to the dark side?
-    A: Anakin Skywalker
-
-2.  Q: What bounty hunter's DNA is the Clone army made from?
-    A: Jango fett
-
-3.  Q: On what planet did Anakin see Padme for the last time?
-    A: Mustafar
-
-4.  Q: Who infiltrates Jabba’s palace in disguise to save Han Solo?
-    A: Princess Leia
-
-5.  Q: Who says, "You can't win, Darth. If you strike me down, I shall become more powerful   than you could possibly imagine"
-    A: Obi-Wan Kenobi
-
-6.  Q: What color is Luke's lightsaber in Return of the Jedi?
-    A: Green
-
-7.  Q: What year did 'The Empire Strikes Back' come out?
-    A: 1980
-
-8.  Q: Who was Darth Sidious' master?
-    A: Darth Plagueis
-
-9.  Q: Which clone trooper served under Jedi Master Plo Koon in the Clone Wars?
-    A: Commander Wolffe
-
-10. Q: How many Dewbacks were in the original 1977 theatrical cut of A New Hope?
-    A: 2
-*/
